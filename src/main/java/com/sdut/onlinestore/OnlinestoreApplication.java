0d99232cd295cb4d297a8fa1e1b961f443bec6d9
Nash <33270266+whywhathow@users.noreply.github.com>
2019-07-11 22:44:00 +0800

    15. finished

--- src/main/java/com/sdut/onlinestore/OnlinestoreApplication.java
+++ src/main/java/com/sdut/onlinestore/OnlinestoreApplication.java
@@ -2,8 +2,14 @@ package com.sdut.onlinestore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
+@EnableTransactionManagement
+@EnableCaching
 public class OnlinestoreApplication {
 
     public static void main(String[] args) {
